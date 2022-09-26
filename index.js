ccf3982fd08d878a5e0700bce544d03f5cbb6691
bazla <dobr1nya@mail.ru>
2022-09-26 03:26:42 +0300

    feat: connect socket.io

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import { createServer } from 'http';
 import { userRouter } from './routes/user.routes.js';
 import { messageRouter } from './routes/message.routes.js';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const server = createServer(app);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,4 +21,26 @@ app.use('/messages', messageRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: 'https://messenger-viktar.herokuapp.com/',
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+  global.chatSocket = socket;
+  socket.on('add-user', (userId) => {
+    onlineusers.set(userId, socket.id);
+  });
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieve', data.msg);
+    }
+  });
 });
